f981c35d90b7d89c62aaaf425438a1e3228416b3
anindra <anindrakarmakar+git@proton.me>
2026-08-25 00:14:48 +0000

    Release 1.0.4 (versionCode 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.anindra.messages"
         minSdk = 29
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.3"
+        versionCode = 8
+        versionName = "1.0.4"
     }
 
     signingConfigs {
