492b38842f35cd1ef50e8facbc8259b2abb660be
Anindra <anindrakarmakar+git@proton.me>
2026-08-27 01:25:49 +0530

    bump versionName to 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.anindra.messages"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.6"
+        versionCode = 11
+        versionName = "1.0.8"
     }
 
     signingConfigs {
