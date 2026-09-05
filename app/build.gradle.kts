effb686b307ed44f6278730377b7bf1788903576
Anindra <anindrakarmakar+git@proton.me>
2026-09-06 00:16:33 +0530

    chore(release): bump version to 1.0.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.anindra.messages"
         minSdk = 29
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.0.19"
+        versionCode = 23
+        versionName = "1.0.20"
     }
 
     signingConfigs {
